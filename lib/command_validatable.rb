ee91b68e937a42dcec6ab2f3ba911e1c9b9c92d1
Jairo Diaz <jairo.diaz@codescrum.com>
2013-07-10 15:18:31 +0100

    Inserted more readable construct

--- lib/command_validatable.rb
+++ lib/command_validatable.rb
@@ -2,12 +2,11 @@ module SimpleImageEditor
 
   #Implements validation methods for a command.
   module CommandValidatable
-
     # Returns true if the arguments is a numbers between 1 and 250.
     # @param args The argument to be checked.
     # @return [boolean].
     def validates_numericality_of(arg)
-      arg.to_i >= 1 and arg.to_i <= 250
+      arg.to_i >= 1 && arg.to_i <= 250
     end
 
     # Returns true if the argument is a valid color (a capital letter).
@@ -23,8 +22,11 @@ module SimpleImageEditor
     def validates_format_for(args)
       return true if self.argument_types.nil?
       args.each_with_index do |arg, index|
-        result = self.argument_types[index] == Integer ? validates_numericality_of(arg)
-                                                             : validates_color_for(arg)
+        result = if self.argument_types[index] == Integer
+          validates_numericality_of(arg)
+        else
+          validates_color_for(arg)
+        end
         return false unless result
       end
       true
