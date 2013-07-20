088b96b1da970b8912b1112d133630c86ca2c9ef
Jairo Diaz <jairo.diaz@codescrum.com>
2013-07-20 17:55:12 +0100

    refactored command_spec

--- spec/unit/command_spec.rb
+++ spec/unit/command_spec.rb
@@ -3,28 +3,18 @@ require 'spec_helper'
 describe SimpleImageEditor::Command do
   let(:my_command) { SimpleImageEditor::Command.new [], lambda{} }
 
+  subject { my_command }
+
+  it { should respond_to :number_of_arguments }
+  it { should respond_to :argument_types }
+  it { should respond_to :transform }
+
   describe "#block" do
-    it "should define a block" do
-      my_command.block = lambda { |image, args| true }
-      expect(my_command.block.call(:image, :args)).to be_true
-    end
-
     it "should be called by transform" do
-        my_command.block = lambda { |image, args| true }
-        my_command.block.should_receive(:call).with(:image, :arg0, :arg1)
-        my_command.transform(:image, :arg0, :arg1)
-    end
-  end
-
-  describe "#number_of_arguments" do
-    it "should respond to number_of_arguments" do
-      expect(my_command).to respond_to :number_of_arguments
-    end
-  end
-
-  describe "#argument_types" do
-    it "should respond to argument_types" do
-      expect(my_command).to respond_to :argument_types
+      block = lambda { |image, args| true }
+      block.should_receive(:call).with(:image, :arg0, :arg1)
+      my_command = SimpleImageEditor::Command.new [], block
+      my_command.transform(:image, :arg0, :arg1)
     end
   end
 
